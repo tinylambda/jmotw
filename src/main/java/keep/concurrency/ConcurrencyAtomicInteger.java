877b80f145482ee7fcb31e9cda44d85e8fee6870
Felix Pan <tinylambda@gmail.com>
2020-04-24 13:05:09 +0800

    Add AtomicInteger test

---
+++ src/main/java/keep/concurrency/ConcurrencyAtomicInteger.java
@@ -0,0 +1,60 @@
+package keep.concurrency;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class ConcurrencyAtomicInteger {
+    private static final Integer THREADS_COUNT = 20;
+    private static Integer nonAtomicRace = 0;
+    private static AtomicInteger atomicRace = new AtomicInteger(0);
+
+    private static void nonAtomicIncrease() {
+        nonAtomicRace += 1;
+    }
+
+    private static void atomicIncrease() {
+        atomicRace.incrementAndGet();
+    }
+
+    public static void main(String[] args) throws Exception {
+        Thread[] threads = new Thread[THREADS_COUNT];
+        for (int i=0; i<THREADS_COUNT; i++) {
+            threads[i] = new Thread(new Runnable() {
+                public void run() {
+                    for(int i=0; i<10000; i++) {
+                        atomicIncrease();
+                    }
+                }
+            });
+        }
+
+        for(Thread thread : threads) {
+            thread.start();
+        }
+
+        for(Thread thread : threads) {
+            thread.join();
+        }
+
+        System.out.println("Atomic increase result: " + atomicRace);
+
+        for (int i=0; i<THREADS_COUNT; i++) {
+            threads[i] = new Thread(new Runnable() {
+                public void run() {
+                    for(int i=0; i<10000; i++) {
+                        nonAtomicIncrease();
+                    }
+                }
+            });
+        }
+
+        for(Thread thread : threads) {
+            thread.start();
+        }
+
+        for(Thread thread : threads) {
+            thread.join();
+        }
+
+        System.out.print("NonAtomic increase result: " + nonAtomicRace);
+    }
+}
