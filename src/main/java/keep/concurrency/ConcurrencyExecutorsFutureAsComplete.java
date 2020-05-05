4716f750f3dafd05888d7f3404b7aae3484e9c8d
Felix Pan <tinylambda@gmail.com>
2020-05-05 13:25:56 +0800

    Add more concurrency

---
+++ src/main/java/keep/concurrency/ConcurrencyExecutorsFutureAsComplete.java
@@ -0,0 +1,34 @@
+package keep.concurrency;
+
+import java.util.Random;
+import java.util.concurrent.*;
+
+public class ConcurrencyExecutorsFutureAsComplete {
+    static class Worker implements Callable<Integer> {
+        private int workerId;
+        public Worker(int id) {
+            this.workerId = id;
+        }
+        @Override
+        public Integer call() throws Exception {
+            Random random = new Random();
+            TimeUnit.SECONDS.sleep(random.nextInt(2));
+            return this.workerId;
+        }
+    }
+    public static void main(String[] args) throws Exception {
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
+        CompletionService<Integer> completionService = new ExecutorCompletionService<Integer>(executorService);
+        for(int i=0; i<10; i++) {
+            completionService.submit(new Worker(i));
+        }
+        executorService.shutdown();
+
+        for(int i=0; i<10; i++) {
+            Integer result = completionService.take().get();
+            if (result != null) {
+                System.out.println(result);
+            }
+        }
+    }
+}
